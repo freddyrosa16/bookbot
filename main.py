b3fa776d752fee2c2d5212ce2f1677c66c3438ff
freddyrosa16 <freddy.rosa16@gmail.com>
2024-06-04 00:48:26 -0400

    Add letter counting functionality to main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     path = "books/frankenstein.txt"
     content = get_book(path)
     word_count = word_counter(content)
-    print(f"The're are {word_count} in the book.")
+    letter_counts = letter_counter(content)
+    print(letter_counts)
 
 
 def get_book(path_to_file):
@@ -13,6 +14,16 @@ def get_book(path_to_file):
 def word_counter(content):
     words = len(content.split())
     return words
+
+
+def letter_counter(content):
+    letter_counts = {}
+    for letter in content.lower():
+        if letter in letter_counts:
+            letter_counts[letter] += 1
+        else:
+            letter_counts[letter] = 1
+    return letter_counts
 
 
 main()
