f9c4bc8f2038823e34361a306f5ab9f8125623a6
freddyrosa16 <freddy.rosa16@gmail.com>
2024-08-29 10:50:47 -0400

    Add word counting functionality

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     reader = readers(path_to_file)
+    word_count = counters(reader)
     print(reader)
+    print(word_count)
 
 
 def readers(path_to_file):
     with open(path_to_file) as f:
         return f.read()
+
+
+def counters(reader):
+    counter = 0
+    words = reader.split()
+    for word in words:
+        counter += 1
+    return counter
 
 
 main()
