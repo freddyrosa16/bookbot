898597891f3f29498eff2b8b318c80c073886f1f
freddyrosa16 <freddy.rosa16@gmail.com>
2024-08-31 12:02:34 -0400

    Refactor character counting logic and improve reporting in main.py

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def main():
     reader = readers(path_to_file)
     word_count = counters(reader)
     letters = character_counter(reader)
-    print(letters)
+    sorted_letters = sort_characters(letters)
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document")
+    for char, count in sorted_letters:
+        print(f"The {char} character was found {count} times")
+    print("--- End report ---")
 
 
 def readers(path_to_file):
@@ -20,11 +25,18 @@ def character_counter(reader):
     character = {}
     for c in reader:
         lowered = c.lower()
-        if lowered not in character:
-            character[lowered] = 1
-        else:
-            character[lowered] += 1
+        if lowered.isalpha():
+            if lowered not in character:
+                character[lowered] = 1
+            else:
+                character[lowered] += 1
     return character
+
+
+def sort_characters(character):
+    character_list = list(character.items())
+    character_list.sort()
+    return character_list
 
 
 main()
