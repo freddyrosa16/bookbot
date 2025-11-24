9c7031563cac5ec8cfefb61c7da911f7aea9b76e
Freddy Rosa <freddy.rosa16@gmail.com>
2025-11-24 13:44:04 -0400

    Add initial implementation of text processing functions

---
+++ stats.py
@@ -0,0 +1,25 @@
+def num_words(text):
+    count = 0
+    splitted_text = text.split()
+    for i in splitted_text:
+        count += 1
+    return count
+
+def letter_counter(text):
+    character_counter = {}
+    splitted_text = text.lower().split()
+    for word in splitted_text:
+        for letter in word:
+            if letter in character_counter:
+                character_counter[letter] += 1
+            else:
+                character_counter[letter] = 1
+    return character_counter
+
+def report(characters):
+    for key, value in characters.items():
+        
+
+def sort_on(items):
+    return items["num"]
+        
